213c9803bae870bcfb1587b2df0b9190f5a1ebc7
Wil Clouser <wclouser@mozilla.com>
2020-06-02 10:48:02 -0700

    feat: Add payments server to our L10n scripts

--- scripts/extract-and-pull-request.sh
+++ scripts/extract-and-pull-request.sh
@@ -1,12 +1,12 @@
-#!/bin/sh
+#!/bin/bash
 
 set -e
 
 rm -rf workspace
 mkdir workspace
 (cd workspace && git clone --depth 1 https://github.com/mozilla/fxa)
-(cd workspace/fxa && yarn workspaces focus fxa-content-server fxa-auth-server)
+(cd workspace/fxa && yarn workspaces focus fxa-content-server fxa-auth-server fxa-payments-server)
 
 # random release number, avoids collision with old trains or branches
 r=$(( $RANDOM + $RANDOM + 1000 ))
-NODE_ENV=development ./scripts/extract_strings.sh --mailer-repo workspace/fxa/packages/fxa-auth-server --content-repo workspace/fxa/packages/fxa-content-server --l10n-repo . $r
+NODE_ENV=development ./scripts/extract_strings.sh --mailer-repo workspace/fxa/packages/fxa-auth-server --payments-repo workspace/fxa/packages/fxa-payments-server --content-repo workspace/fxa/packages/fxa-content-server --l10n-repo . $r
