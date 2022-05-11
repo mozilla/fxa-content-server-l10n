b31a66852963f027870fa25e3893e9d390215118
Francesco Lodolo <flodolo@mozilla.com>
2022-05-11 16:11:41 +0200

    Use GH Actions for automation (#579)

--- scripts/ci-creds.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-
-fs.writeFileSync('.gh.json', JSON.stringify({
-  github_token: process.env.GITHUB_TOKEN,
-  github_user: process.env.GITHUB_USER,
-  use_editor: false,
-}));
