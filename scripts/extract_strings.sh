cfa676031281f1bc22b031e1d0068087351ad345
Francesco Lodolo <flodolo@mozilla.com>
2022-05-12 18:10:35 +0200

    Make GetText extraction less noisy (#581)

--- scripts/extract_strings.sh
+++ scripts/extract_strings.sh
@@ -76,21 +76,53 @@ set -x
 
 
 # Gettext extraction
-(cd $CONTENT_DIR && npx grunt l10n-extract)
-cp -r $CONTENT_DIR/locale/templates/* $L10N_DIR/locale/templates
 
-# see https://github.com/mozilla/zippy/blob/180f746cd84cb3e409c76649e810485acb4512dc/locale/omg_new_l10n.sh#L90
-# also see https://bugzilla.mozilla.org/show_bug.cgi?id=1168488
-msgfilter -i $L10N_DIR/locale/sr/LC_MESSAGES/client.po -o $L10N_DIR/locale/sr_Latn/LC_MESSAGES/client.po recode-sr-latin
-msgfilter -i $L10N_DIR/locale/sr/LC_MESSAGES/server.po -o $L10N_DIR/locale/sr_Latn/LC_MESSAGES/server.po recode-sr-latin
+# Extract the hash of the pot files content without the POT-Creation-Date line.
+# This is used to avoid copying over the templates if the actual content
+# was not updated by the l10n-extract script
+OLD_HASHES=$(python $L10N_DIR/scripts/pot_checksum.py $L10N_DIR/locale/templates/)
+(cd $CONTENT_DIR && npx grunt l10n-extract)
+NEW_HASHES=$(python $L10N_DIR/scripts/pot_checksum.py $L10N_DIR/locale/templates/)
+if [ "$NEW_HASHES" = "$OLD_HASHES" ];
+then
+    echo ".pot files have no updated content"
+else
+    cp -r $CONTENT_DIR/locale/templates/* $L10N_DIR/locale/templates
+
+    # Serbian conversion from Cyrillic to Latin script
+    # See https://github.com/mozilla/zippy/blob/180f746cd84cb3e409c76649e810485acb4512dc/locale/omg_new_l10n.sh#L90
+    # Also see https://bugzilla.mozilla.org/show_bug.cgi?id=1168488
+    msgfilter -i $L10N_DIR/locale/sr/LC_MESSAGES/client.po -o $L10N_DIR/locale/sr_Latn/LC_MESSAGES/client.po recode-sr-latin
+    msgfilter -i $L10N_DIR/locale/sr/LC_MESSAGES/server.po -o $L10N_DIR/locale/sr_Latn/LC_MESSAGES/server.po recode-sr-latin
+    sed -i'' -e 's/Language: sr/Language: sr_Latn/g' "$L10N_DIR/locale/sr_Latn/LC_MESSAGES/client.po"
+    sed -i'' -e 's/Language: sr/Language: sr_Latn/g' "$L10N_DIR/locale/sr_Latn/LC_MESSAGES/server.po"
+
+    # Merge updates to all locales
+    cd $L10N_DIR
+    ./scripts/merge_po.sh ./locale
+fi
 
 # Fluent extraction
 cp $PAYMENTS_DIR/public/locales/en-US/*.ftl $L10N_DIR/locale/templates
 cp $SETTINGS_DIR/public/locales/en-US/*.ftl $L10N_DIR/locale/templates
 cp $MAILER_DIR/public/locales/en/*.ftl $L10N_DIR/locale/templates
 
-cd $L10N_DIR
-./scripts/merge_po.sh ./locale
-./scripts/before_prod_deploy.sh --l10n-repo $L10N_DIR
+# Some locales must be copied over to a different locale code
+# See details in https://github.com/mozilla/fxa-content-server-l10n/pull/79#issuecomment-149281761
+cp -r $L10N_DIR/locale/sv_SE/* $L10N_DIR/locale/sv
+sed -i'' -e 's/Language: sv_SE/Language: sv/g' "$L10N_DIR/locale/sv/LC_MESSAGES/client.po"
+sed -i'' -e 's/Language: sv_SE/Language: sv/g' "$L10N_DIR/locale/sv/LC_MESSAGES/server.po"
+
+cp -r $L10N_DIR/locale/hi_IN/* $L10N_DIR/locale/hi
+sed -i'' -e 's/Language: hi_IN/Language: hi/g' "$L10N_DIR/locale/hi/LC_MESSAGES/client.po"
+sed -i'' -e 's/Language: hi_IN/Language: hi/g' "$L10N_DIR/locale/hi/LC_MESSAGES/server.po"
+
+cp -r $L10N_DIR/locale/pt_PT/* $L10N_DIR/locale/pt
+sed -i'' -e 's/Language: pt_PT/Language: pt/g' "$L10N_DIR/locale/pt/LC_MESSAGES/client.po"
+sed -i'' -e 's/Language: pt_PT/Language: pt/g' "$L10N_DIR/locale/pt/LC_MESSAGES/server.po"
+
+cp -r $L10N_DIR/locale/fy_NL/* $L10N_DIR/locale/fy
+sed -i'' -e 's/Language: fy_NL/Language: fy/g' "$L10N_DIR/locale/fy/LC_MESSAGES/client.po"
+sed -i'' -e 's/Language: fy_NL/Language: fy/g' "$L10N_DIR/locale/fy/LC_MESSAGES/server.po"
 
 set +x
